8c336387268caac6f5d1907e70b1a95223ff2561
Joel <joelmatos@live.com>
2024-09-06 20:00:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,51 @@
+let dados = [
+    {
+        titulo: "Hu Tao",
+        descricao: `Hutao (inglês: Hu Tao; chinês: 胡桃 Hú Táo, "Noz") é um personagem Pyro jogável em Genshin Impact. Ela é a 77ª Diretora da Funerária Wangsheng. Por algum motivo o nome da personagem em português é composto de uma única palavra, na maior partes das línguas ele é composto de duas palavras.`,
+        link: "https://keqingmains.com/hu-tao/",
+        elemento: "Pyro",
+        tags: "hu tao, pyro, dps, goat"
+    },
+    {
+        titulo: "Neuvillette",
+        descricao: "Neuvillette é um personagem Hydro jogável em Genshin Impact. Neuvillette é o Iudex de Fontaine , e o líder do Fantasma de Marechaussee. Embora Neuvillette cumpra as regras do tribunal com a maior reverência, ele é bastante indiferente ao lidar com as emoções humanas e muitas vezes se distancia dos olhos do público.",
+        link: "https://keqingmains.com/furina/",
+        elemento: "Hydro",
+        tags: "hydro"
+    },
+    {
+        titulo: "Alhaitham",
+        descricao: "Alhaitham é um personagem Dendro jogável em Genshin Impact. O atual Escriba da Academia de Sumeru, um homem de grande inteligência e talento. Vive sua vida livre e despreocupado, sendo uma pessoa muito difícil de se encontrar.  ",
+        link: "https://keqingmains.com/q/alhaitham-quickguide/",
+        elemento: "Dendro",
+        tags: "dendro"
+    },
+    {
+        titulo: "Navia",
+        descricao: "Navia é um personagem Geo jogável de Genshin Impact. Ela é a otimista presidente da Spina di Rosula, um cargo anteriormente ocupado por seu falecido pai, Callas. Seu espírito entusiasmado e tenaz permitem que ela continue ajudando o povo de Fontaine, mesmo diante das várias perdas em sua vida.",
+        link: "https://keqingmains.com/q/navia-quickguide/",
+        elemento: "Geo",
+        tags: "geo"
+    },
+    {
+        titulo: "Xiao",
+        descricao: `Xiao (chinês: 魈 Xiāo, "Demônio"), Nome de Yaksha Alatus, é um personagem Anemo jogável de Genshin Impact. É um adeptus e o único membro restante dos Cinco Yakshas enviados por Morax para subjugar os espíritos demoníacos que atormentavam Liyue. Ele atualmente mora no Albergue Wangshu, onde se isola das multidões e de interações humanas.`,
+        link: "https://keqingmains.com/xiao/",
+        elemento: "Anemo",
+        tags: "anemo"
+    },
+    {
+        titulo: "Clorinde",
+        descricao: "Clorinde é um personagem Electro jogável de Genshin Impact. Ela é a mais forte Campeã Duelista em Fontaine. Ela já serviu como guarda-costas de Furina e tem uma história com Navia.",
+        link: "https://keqingmains.com/q/clorinde-quickguide/",
+        elemento: "Electro",
+        tags: "electro"
+    },
+    {
+        titulo: "Wriothesley",
+        descricao: "Wriothesley é um personagem Cryo jogável de Genshin Impact. endo matado seus pais adotivos abusivos para salvar seus irmãos adotivos, Wriothesley foi condenado e exilado para a Fortaleza de Meropide em sua adolescência. Ele acabou se tornando seu Administrador e implementou uma série de reformas durante seu governo, servindo como modelo para os prisioneiros.",
+        link: "https://keqingmains.com/q/wriothesley-quickguide/",
+        elemento: "Cryo",
+        tags: "cryo"
+    },
+];
